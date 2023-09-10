870693407c167f6f665bd5d944d9361e63725788
Khalifa Fadel <fffadel19982@gmail.com>
2023-09-11 00:42:42 +0100

    first

--- app.py
+++ app.py
@@ -6,27 +6,14 @@ from lib.database_connection import get_flask_database_connection
 app = Flask(__name__)
 
 # == Your Routes Here ==
+@app.route('/goodbye')
+def get_goodbye():
+    return render_template('goodbye.html')
 
+@app.route("/hello")
+def get_hello():
+    return render_template("hello.html", message="Hello, worldewjhfo3!")
 
-# == Example Code Below ==
-
-# GET /emoji
-# Returns a smiley face in HTML
-# Try it:
-#   ; open http://localhost:5000/emoji
-@app.route('/emoji', methods=['GET'])
-def get_emoji():
-    # We use `render_template` to send the user the file `emoji.html`
-    # But first, it gets processed to look for placeholders like {{ emoji }}
-    # These placeholders are replaced with the values we pass in as arguments
-    return render_template('emoji.html', emoji=':)')
-
-# This imports some more example routes for you to see how they work
-# You can delete these lines if you don't need them.
-from example_routes import apply_example_routes
-apply_example_routes(app)
-
-# == End Example Code ==
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
